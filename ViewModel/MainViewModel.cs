7e45661eb50ed70278a34fb3ba6404c29b6bf4d7
Mulleriano <igormullersantos@hotmail.com>
2023-10-02 16:19:47 -0300

    Cria classe MainViewModel, com parâmetros para biding e comandos para adicionar tarefas

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Tasks.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    ObservableCollection<String> items;
+
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+        Items.Add(Text);
+        Text = string.Empty;
+    }
+}
