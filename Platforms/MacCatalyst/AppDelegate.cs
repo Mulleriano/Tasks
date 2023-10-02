d48ecf907e5a9bf7c545b9ea794c9715b4766845
mulleriano <igor.santos@IT4-NBK-WIN-90>
2023-10-02 14:10:09 -0300

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tasks
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
