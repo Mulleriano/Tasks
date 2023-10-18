94afda45a2acf5a0345d40bdaa7cfc64f7b54337
Mulleriano <igormullersantos@hotmail.com>
2023-10-18 10:26:56 -0300

    Rename MainViewModel to TaskViewModel

--- ViewModel/TaskViewModel.cs
+++ ViewModel/TaskViewModel.cs
@@ -4,11 +4,11 @@ using System.Collections.ObjectModel;
 
 namespace Tasks.ViewModel;
 
-public partial class MainViewModel : ObservableObject
+public partial class TaskViewModel : BaseViewModel
 {
     IConnectivity connectivity;
 
-    public MainViewModel(IConnectivity connectivity)
+    public TaskViewModel(IConnectivity connectivity)
     {
         Items = new ObservableCollection<string>();
         this.connectivity = connectivity;
@@ -46,6 +46,6 @@ public partial class MainViewModel : ObservableObject
     [RelayCommand]
     async Task Tap(string s)
     {
-        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        await Shell.Current.GoToAsync($"{nameof(TasksPage)}?Text={s}");
     }
 }
