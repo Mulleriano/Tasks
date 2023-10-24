c522cc2831d43253db1f258f87da1c045e574aac
Mulleriano <igormullersantos@hotmail.com>
2023-10-24 11:36:59 -0300

    Add viewmodel context to appshell.xaml.cs

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace Tasks;
+﻿using Tasks.ViewModel;
+
+namespace Tasks;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        this.BindingContext = new AppShellViewModel();
 
         Routing.RegisterRoute(nameof(TasksPage), typeof(TasksPage));
         Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
