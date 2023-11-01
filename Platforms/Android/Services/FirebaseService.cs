f69b87d0dd7f4cbc00756f5839711c3042b28030
Mulleriano <igormullersantos@hotmail.com>
2023-11-01 16:35:40 -0300

    Add Firebase Service with Firebase Cloud Messaging

---
+++ Platforms/Android/Services/FirebaseService.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Content;
+using AndroidX.Core.App;
+using Firebase.Messaging;
+
+namespace Tasks.Platforms.Android.Services
+{
+    [Service(Exported = true)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseService : FirebaseMessagingService
+    {
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            if (Preferences.ContainsKey("DeviceToken"))
+            {
+                Preferences.Remove("DeviceToken");
+            }
+            Preferences.Set("DeviceToken", token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+
+            var notification = message.GetNotification();
+
+            SendNotification(notification.Body, notification.Title, message.Data);
+        }
+
+        private void SendNotification(string messageBody, string title, IDictionary<string, string> data)
+        {
+
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            intent.AddFlags(ActivityFlags.SingleTop);
+
+
+            foreach (var key in data.Keys)
+            {
+                string value = data[key];
+                intent.PutExtra(key, value);
+            }
+
+            var pendingIntent = PendingIntent.GetActivity(this,
+                MainActivity.NotificationID, intent, PendingIntentFlags.OneShot | PendingIntentFlags.Immutable);
+
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.Channel_ID)
+                .SetContentTitle(title)
+                .SetContentText(messageBody)
+                .SetChannelId(MainActivity.Channel_ID)
+                .SetSmallIcon(Resource.Mipmap.appicon)
+                .SetContentIntent(pendingIntent)
+                .SetAutoCancel(true)
+                .SetPriority((int)NotificationPriority.Max);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(MainActivity.NotificationID, notificationBuilder.Build());
+        }
+    }
+}
