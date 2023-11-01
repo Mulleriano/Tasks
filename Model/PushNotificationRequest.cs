cafa990b1b5f608d0cddba40428dbbd376c0aef5
Mulleriano <igormullersantos@hotmail.com>
2023-11-01 16:32:56 -0300

    Create notification Models, received and requested

---
+++ Model/PushNotificationRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace PushNotification.Model
+{
+    public class PushNotificationRequest
+    {
+        public List<string> registration_ids { get; set; } = new List<string>();
+        public NotificationMessageBody notification { get; set; }
+        public object data { get; set; }
+    }
+
+    public class NotificationMessageBody
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
