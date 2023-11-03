39b56db5a6c5ce6e567ee15124bb050b96f85890
Mulleriano <igormullersantos@hotmail.com>
2023-11-03 10:59:22 -0300

    Add WebView with url string

---
+++ ViewModel/WebviewViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tasks.ViewModel
+{
+    public partial class WebviewViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        string webUrl = "https://github.com/Mulleriano";
+    }
+}
